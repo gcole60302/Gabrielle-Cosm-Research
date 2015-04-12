2f4ec4d53e7e22958e3f9c4c4c9e1a6cc8f54f7f
Gabrielle Cole <gcole60302@gmail.com>
2015-04-12 12:08:00 -0500

    Generalized Form of EFACT
    So the is the form that works, returns an array of n values which modify
    the Hubble constant

--- Python EFACT Redo.py
+++ Python EFACT Redo.py
@@ -5,17 +5,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math as m
 
-#Some constants
-OmmM = 0.27
-OmmK =
-OmmL =
+#Some constants, n is the number of data points
+OmmM = 0.27 * np.ones(n, dtype=float)
+OmmK = x1 * np.ones(n, dtype=float)
+OmmL = x2 * np.ones(n, dtype=float)
 
 #Array of redshift values for clusters
-z = np.arange(1, n)
+z = np.arange(1, n, dtype=float)
 
 #Define empty set for final output
-EFFECT = np.zeros(n)
+EFFECT = np.zeros(n, dtype=float)
 
-for i in range(len(z)):
-    EFFECT[i] = m.sqrt(OmmM(1+z[i])**3 + OmmK(1 +z)**2 + OmmL)
+for i in range(0,n-1):
+    EFFECT[i] = (OmmM[i])*(1+z[i])**3 + (OmmK[i])*(1 +z[i])**2 + (OmmL[i])
+    
+
+print EFFECT
 
--- Random Practice.py
+++ Random Practice.py
@@ -6,17 +6,19 @@ import numpy as np
 import math as m
 
 #Some constants
-OmmM = 0.27 * np.ones(100, dtype=object)
-OmmK = 4 * np.ones(100, dtype=object)
-OmmL = 9 * np.ones(100, dtype=object)
+OmmM = 0.27 * np.ones(100, dtype=float)
+OmmK = 4 * np.ones(100, dtype=float)
+OmmL = 9 * np.ones(100, dtype=float)
 
 #Array of redshift values for clusters
-z = np.arange(1, 100, dtype=object)
+z = np.arange(1, 100, dtype=float)
 
 #Define empty set for final output
-EFFECT = np.zeros(100, dtype=object)
+EFFECT = np.zeros(100, dtype=float)
 
-for i in range(len(z)):
-    EFFECT[i] = (OmmM[i])*(1+z[i])**3 + (OmmK[i])*(1 +z)**2 + (OmmL[i])
+for i in range(0,99):
+    EFFECT[i] = (OmmM[i])*(1+z[i])**3 + (OmmK[i])*(1 +z[i])**2 + (OmmL[i])
+    
 
 print EFFECT
+
