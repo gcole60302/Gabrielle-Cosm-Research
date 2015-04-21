e666545424455d4d3d0062fbd0a4b85e103c4e2c
Gabrielle Cole <gcole60302@gmail.com>
2015-04-20 21:21:32 -0500

    Fooling around

--- Random Practice2.py
+++ Random Practice2.py
@@ -6,39 +6,256 @@ import math
 from sympy import *
 import scipy.optimize as opt
 from scipy import integrate
+import scipy.optimize as opt
+from matplotlib.ticker import NullFormatter, MaxNLocator
+from numpy import linspace
+plt.ion()
 
+#Here's a whole bunch of constants
+H0=72. #km / (s*Mpc)
+OmegaM= 0.27 #matter density
+hubble = H0/100.
+hubble70 = H0/70.
+mmw = 0.59 #mean molecular weight of gas
+mu_e = 1.143 #mean molecular weight of electrons in cluster
+m_p =1.6726e-27 #proton mass in kg
+m_e =9.11e-31 #electron mass in kg
+mpc = 3.08568025e22   #megaparsec in meters
+Msol = 1.98892e30 #solar masses in kg
+sigma_T = (6.652e-25)/(100.)**2 #thompson cross section in m^2
+k_b= 1.3806503e-23 #Blotsz const in m^2*kg*s^-2*K^-1
+c = 3.0e8 #speed of light in m*s^-1
+q= 1.60217646e-19 #unit of charge in joules equal to electron volt
+e_rm = ((m_e)*(c)**2)/((1000)*(q)) #rest mass energy of electron in KeV
+y_const = (sigma_T)/(e_rm) # compt. parameter constant
 G= 4.30117902e-9 # gravitational constant, km^2*Mpc*s^-2
 
+###################################################################
+
+#Arnaud profile constants
+P0 = 8.403/ (hubble70)**(1.5) #Pressure nought
+alpha = 1.0510
+beta = 5.4905
+gamma = 0.3081
+index = (beta - gamma) / alpha
+c500 = 1.177 #charcteristic concentration
+alpha_p = 0.12 #constant of correction see Arnaud (2010)
+
+###################################################################
+
+#Here are some functions we need 
 def Rho_Crit(z):
     return ((((np.sqrt((0.27)*(1. + z)**(3) + (1. - 0.27))))**(2))*(3.)*((72.)**(2.)))/((8.)*(np.pi)*(G))
 
-def PROFILE(z, M500):
-    return((M500)/((2500./3.)*(np.pi)*(Rho_Crit(z))))**(1./3.)
-    
+def E_FACT(z):
+    return np.sqrt((0.27)*(1. + z)**(3) + (1. - 0.27))
 
+def ANG_DIAM_DIST(z):
+    return(((3e8)/(72))*(scipy.integrate.romberg(E_FACT, 0, z)))/(1 + z)
 
-#def Plot(x,y):
- #   a = x*np.arange(20)
-  #  b = y*np.arange(20)
-   # plt.plot(a,b)
-    #return plt.show()
+def PROFILE1(z, M500):
+    R500 = ((M500)/((2500./3.)*(np.pi)*(Rho_Crit(z))))**(1./3.)
+    x = np.arange(0,(100.)*(9.)*(R500)/(100.), 0.001)
+    q = np.zeros(len(x))
+    for i in range(len(x)):
+        y1= x[i]
+        r = y1
+        upperlim = np.sqrt(((9.)*(R500))**(2.) - (r)**(2.))
+        def ARNAUD_PROJ(x1):
+            return (1.)/(((((c500/R500)**2)*((x1**2. + y1**2.)))**(gamma/2.))*((1 + (((c500/R500)**2.)*(x1**2. + y1**2.))**(alpha/2))**(index)))
+        if r < (9.)*(R500):
+            q[i] = scipy.integrate.romberg(ARNAUD_PROJ,0.001, upperlim, divmax=20)
+        elif r >(9.)*(R500):
+            break
+    PNORM = (1.65e-3)*((E_FACT(z))**(8./3.))*((((hubble70)*(M500))/(3.0e14))**(2./3. + alpha_p))*((hubble70)**2.)*((8.403)/(hubble70)**(3./2.))*(1e6)
+    c = ((x)*(c500))/(R500)
+    f = (y_const)*(q)*(PNORM)*(2.)*(mpc)
+    r_over_r500= (c)/((c500)*(R500))
+    absy_150ghz = f
+    r_arcmin =(r_over_r500)/(ANG_DIAM_DIST(z))*(180.)/(np.pi)*(60.)
+    dT_uK = (absy_150ghz)*(1.0e6)*(2.73)
+    #plt.plot(r_arcmin, dT_uK)
+    #plt.ylabel('Temperature (uK)')
+    #plt.xlabel('Radial Distance (Arcmin)')
+    #plt.title('Temp as a Function of Arcmin')
+    #plt.yscale('log')
+    return dT_uK 
 
-#def E_FACT(z):
- #   return np.sqrt((0.27)*(1. + z)**(3) + (1. - 0.27))
+def PROFILE2(z, M500):
+    R500 = ((M500)/((2500./3.)*(np.pi)*(Rho_Crit(z))))**(1./3.)
+    x = np.arange(0,(100.)*(9.)*(R500)/(100.), 0.001)
+    q = np.zeros(len(x))
+    for i in range(len(x)):
+        y1= x[i]
+        r = y1
+        upperlim = np.sqrt(((9.)*(R500))**(2.) - (r)**(2.))
+        def ARNAUD_PROJ(x1):
+            return (1.)/(((((c500/R500)**2)*((x1**2. + y1**2.)))**(gamma/2.))*((1 + (((c500/R500)**2.)*(x1**2. + y1**2.))**(alpha/2))**(index)))
+        if r < (9.)*(R500):
+            q[i] = scipy.integrate.romberg(ARNAUD_PROJ,0.001, upperlim, divmax=20)
+        elif r >(9.)*(R500):
+            break
+    PNORM = (1.65e-3)*((E_FACT(z))**(8./3.))*((((hubble70)*(M500))/(3.0e14))**(2./3. + alpha_p))*((hubble70)**2.)*((8.403)/(hubble70)**(3./2.))*(1e6)
+    c = ((x)*(c500))/(R500)
+    f = (y_const)*(q)*(PNORM)*(2.)*(mpc)
+    r_over_r500= (c)/((c500)*(R500))
+    absy_150ghz = f
+    r_arcmin =(r_over_r500)/(ANG_DIAM_DIST(z))*(180.)/(np.pi)*(60.)
+    dT_uK = (absy_150ghz)*(1.0e6)*(2.73)
+    #plt.plot(r_arcmin, dT_uK)
+    #plt.ylabel('Temperature (uK)')
+    #plt.xlabel('Radial Distance (Arcmin)')
+    #plt.title('Temp as a Function of Arcmin')
+    #plt.yscale('log')
+    return r_arcmin
 
-#def ANG_DIAM_DIST(z):
- #   return(((3e8)/(72.))*(scipy.integrate.romberg(E_FACT, 0, z)))/(1. + z)
-
-#def E_z(z,Omm_M):
- #   return np.sqrt((Omm_M)*(1. + z)**(3) + (1. - Omm_M))
-
-#def Rho_Crit(z, H0, Omm_M):
- #   return (((E_z(z, Omm_M))**(2))*(3.)*((H0)**(2.)))/((8.)*(np.pi)*(G))
-#y = np.arange(1,5,0.01)
-#x = np.arange(2,43, 1)
-#for i in range(3):
- #   print E_z(y[i], x[i])
-    
-#def PROFILE(z, M500):
-    
-    
+# Define a function to make the ellipses
+def ellipse(ra,rb,ang,x0,y0,Nb=100):
+    xpos,ypos=x0,y0
+    radm,radn=ra,rb
+    an=ang
+    co,si=np.cos(an),np.sin(an)
+    the=linspace(0,2*np.pi,Nb)
+    X=radm*np.cos(the)*co-si*radn*np.sin(the)+xpos
+    Y=radm*np.cos(the)*si+co*radn*np.sin(the)+ypos
+    return X,Y
+ 
+# Define the x and y data 
+# For example just using random numbers
+x = PROFILE2(1.3,1e13)
+y = PROFILE1(1.3,1e13)
+ 
+# Set up default x and y limits
+xlims = [min(x),max(x)]
+ylims = [min(y),max(y)]
+ 
+# Set up your x and y labels
+xlabel = '$\mathrm{Your\\ X\\ Label}$'
+ylabel = '$\mathrm{Your\\ Y\\ Label}$'
+ 
+# Define the locations for the axes
+left, width = 0.12, 0.55
+bottom, height = 0.12, 0.55
+bottom_h = left_h = left+width+0.02
+ 
+# Set up the geometry of the three plots
+rect_temperature = [left, bottom, width, height] # dimensions of temp plot
+rect_histx = [left, bottom_h, width, 0.25] # dimensions of x-histogram
+rect_histy = [left_h, bottom, 0.25, height] # dimensions of y-histogram
+ 
+# Set up the size of the figure
+fig = plt.figure(1, figsize=(9.5,9))
+ 
+# Make the three plots
+axTemperature = plt.axes(rect_temperature) # temperature plot
+axHistx = plt.axes(rect_histx) # x histogram
+axHisty = plt.axes(rect_histy) # y histogram
+ 
+# Remove the inner axes numbers of the histograms
+nullfmt = NullFormatter()
+axHistx.xaxis.set_major_formatter(nullfmt)
+axHisty.yaxis.set_major_formatter(nullfmt)
+ 
+# Find the min/max of the data
+xmin = min(xlims)
+xmax = max(xlims)
+ymin = min(ylims)
+ymax = max(y)
+ 
+# Make the 'main' temperature plot
+# Define the number of bins
+nxbins = 50
+nybins = 50
+nbins = 100
+ 
+xbins = linspace(start = xmin, stop = xmax, num = nxbins)
+ybins = linspace(start = ymin, stop = ymax, num = nybins)
+xcenter = (xbins[0:-1]+xbins[1:])/2.0
+ycenter = (ybins[0:-1]+ybins[1:])/2.0
+aspectratio = 1.0*(xmax - 0)/(1.0*ymax - 0)
+ 
+H, xedges,yedges = np.histogram2d(y,x,bins=(ybins,xbins))
+X = xcenter
+Y = ycenter
+Z = H
+ 
+# Plot the temperature data
+cax = (axTemperature.imshow(H, extent=[xmin,xmax,ymin,ymax],
+       interpolation='nearest', origin='lower',aspect=aspectratio))
+ 
+# Plot the temperature plot contours
+contourcolor = 'white'
+xcenter = np.mean(x)
+ycenter = np.mean(y)
+ra = np.std(x)
+rb = np.std(y)
+ang = 0
+ 
+X,Y=ellipse(ra,rb,ang,xcenter,ycenter)
+axTemperature.plot(X,Y,"k:",ms=1,linewidth=2.0)
+axTemperature.annotate('$1\\sigma$', xy=(X[15], Y[15]), xycoords='data',xytext=(10, 10),
+                       textcoords='offset points', horizontalalignment='right',
+                       verticalalignment='bottom',fontsize=25)
+ 
+X,Y=ellipse(2*ra,2*rb,ang,xcenter,ycenter)
+axTemperature.plot(X,Y,"k:",color = contourcolor,ms=1,linewidth=2.0)
+axTemperature.annotate('$2\\sigma$', xy=(X[15], Y[15]), xycoords='data',xytext=(10, 10),
+                       textcoords='offset points',horizontalalignment='right',
+                       verticalalignment='bottom',fontsize=25, color = contourcolor)
+ 
+X,Y=ellipse(3*ra,3*rb,ang,xcenter,ycenter)
+axTemperature.plot(X,Y,"k:",color = contourcolor, ms=1,linewidth=2.0)
+axTemperature.annotate('$3\\sigma$', xy=(X[15], Y[15]), xycoords='data',xytext=(10, 10),
+                       textcoords='offset points',horizontalalignment='right',
+                       verticalalignment='bottom',fontsize=25, color = contourcolor)
+ 
+#Plot the axes labels
+axTemperature.set_xlabel(xlabel,fontsize=25)
+axTemperature.set_ylabel(ylabel,fontsize=25)
+ 
+#Make the tickmarks pretty
+ticklabels = axTemperature.get_xticklabels()
+for label in ticklabels:
+    label.set_fontsize(18)
+    label.set_family('serif')
+ 
+ticklabels = axTemperature.get_yticklabels()
+for label in ticklabels:
+    label.set_fontsize(18)
+    label.set_family('serif')
+ 
+#Set up the plot limits
+axTemperature.set_xlim(xlims)
+axTemperature.set_ylim(ylims)
+ 
+#Set up the histogram bins
+xbins = np.arange(xmin, xmax, (xmax-xmin)/nbins)
+ybins = np.arange(ymin, ymax, (ymax-ymin)/nbins)
+ 
+#Plot the histograms
+axHistx.hist(x, bins=xbins, color = 'blue')
+axHisty.hist(y, bins=ybins, orientation='horizontal', color = 'red')
+ 
+#Set up the histogram limits
+axHistx.set_xlim( min(x), max(x) )
+axHisty.set_ylim( min(y), max(y) )
+ 
+#Make the tickmarks pretty
+ticklabels = axHistx.get_yticklabels()
+for label in ticklabels:
+    label.set_fontsize(12)
+    label.set_family('serif')
+ 
+#Make the tickmarks pretty
+ticklabels = axHisty.get_xticklabels()
+for label in ticklabels:
+    label.set_fontsize(12)
+    label.set_family('serif')
+ 
+#Cool trick that changes the number of tickmarks for the histogram axes
+axHisty.xaxis.set_major_locator(MaxNLocator(4))
+axHistx.yaxis.set_major_locator(MaxNLocator(4))
+ 
+#Show the plot
+plt.draw()
+ 
